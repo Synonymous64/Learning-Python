54eb588f29c7a385aac8c521ff80aa9d88e3ce43
Synonymous64 <prajwalurkude007@gmail.com>
2025-10-28 21:05:13 +0100

    modules

--- 01_Basics/hello_world.py
+++ 01_Basics/hello_world.py
@@ -4,3 +4,10 @@ def greet(n):
     print(n)
 
 greet("Hello, World!")
+
+# this module will have this 4 attributes
+greet1 = "Good Morning"
+greet2 = "Good Afternoon"
+greet3 = "Good Evening"
+greet4 = "Good Night"
+

e2662d9cd249bf42aaf3329faf3b7cd46d3636d8
Synonymous64 <prajwalurkude007@gmail.com>
2025-11-14 20:04:52 +0100

    Tuple features

---
+++ 01_Basics/tuple.py
@@ -0,0 +1,29 @@
+tea_types = ("Black", "Green", "Oolong")
+print(tea_types)
+
+print(tea_types[-1])
+
+# this won't work for assigning the items
+# tea_types[0] = "Lemon"
+# print(tea_types)
+
+print(len(tea_types))
+
+more_tea = ("Herbal", "Earl Grey")
+all_tea = tea_types + more_tea
+
+print(all_tea)
+
+if "Green" in all_tea:
+    print("I have the Green Tea")
+
+more_tea = ["Herbal", "Earl Gray", "Herbal"]
+print(more_tea)
+
+# to get the count of the item
+print(more_tea.count("Herbal"))
+
+# it will assigned the values of the tea_types more like destructuring
+(black, green, Oolong) = tea_types
+print(black)
+
